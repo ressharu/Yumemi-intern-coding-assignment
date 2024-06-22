0466191f672d4c7a56055b12dbc8b0b57cb31da8
haruki9148 <haruki20050202@icloud.com>
2024-06-23 02:08:49 +0900

    占い結果の詳細画面を作成(DetailView)
    - DetailViewの作成
    - カード?を押したらDetailViewに飛ぶようにした(HistoryView)

---
+++ Yumemi-intern-coding-assignment/DetailView.swift
@@ -0,0 +1,65 @@
+//
+//  DetailView.swift
+//  Yumemi-intern-coding-assignment
+//
+//  Created by 渡邉華輝 on 2024/06/23.
+//
+
+import SwiftUI
+import SDWebImageSwiftUI
+
+struct DetailView: View {
+    var personalRecord: PersonalRecordHistory?
+    var fortune: FortuneResponseHistory
+
+    var body: some View {
+        ScrollView {
+            VStack {
+                if let record = personalRecord {
+                    VStack {
+                        Text("入力した情報") // 入力した情報の見出し
+                            .font(.title2)
+                            .padding(.bottom)
+                            .bold()
+                        HStack {
+                            Text("お名前:") // 名前のラベル
+                                .bold()
+                            Text(record.name) // 名前の値
+                        }
+                        HStack {
+                            Text("お誕生日:") // 誕生日のラベル
+                                .bold()
+                            Text("\(record.year)年 \(record.month)月 \(record.day)日") // 誕生日の値
+                        }
+                        HStack {
+                            Text("血液型:") // 血液型のラベル
+                                .bold()
+                            Text(record.bloodType) // 血液型の値
+                        }
+                        HStack {
+                            Text("占った日:") // 占いをした日のラベル
+                                .bold()
+                            Text(record.today) // 占いをした日の値
+                        }
+                    }
+                    .padding()
+                }
+
+                Divider()
+                    .padding(.vertical)
+
+                // FortuneViewを使用して占い結果を表示
+                FortuneView(fortune: FortuneResponse(
+                    name: fortune.name,
+                    capital: fortune.capital,
+                    citizen_day: FortuneResponse.CitizenDay(month: fortune.CitizenDay_month, day: fortune.CitizenDay_day),
+                    has_coast_line: fortune.has_coast_line,
+                    logo_url: fortune.logo_url,
+                    brief: fortune.brief
+                ))
+            }
+            .navigationBarTitle("詳細ビュー", displayMode: .inline) // ナビゲーションバーのタイトルを設定
+            .padding()
+        }
+    }
+}
--- Yumemi-intern-coding-assignment/HistoryView.swift
+++ Yumemi-intern-coding-assignment/HistoryView.swift
@@ -11,74 +11,60 @@ import SwiftData
 import SDWebImageSwiftUI
 
 struct HistoryView: View {
-    // @Queryプロパティラッパーを使用して、データベースからPersonalRecordHistoryのレコードを取得
     @Query(sort: \PersonalRecordHistory.today, order: .reverse) private var personalRecords: [PersonalRecordHistory]
-    // @Queryプロパティラッパーを使用して、データベースからFortuneResponseHistoryのレコードを取得
     @Query(sort: \FortuneResponseHistory.name) private var fortuneResponses: [FortuneResponseHistory]
+    
+    @State private var selectedRecord: FortuneResponseHistory?
+    @State private var showingDetail = false
 
     var body: some View {
         NavigationStack {
             VStack {
-                // タイトルの表示
                 Text("占い履歴！")
                     .font(.title)
                     .padding()
 
-                // リストビューの表示
                 List {
-                    /*
-                    // Personal Recordsセクション
-                    Section(header: Text("Personal Records")) {
-                        // personalRecordsの各レコードを表示
-                        ForEach(personalRecords) { record in
-                            VStack(alignment: .leading) {
-                                Text(record.name) // 名前の表示
-                                    .font(.headline)
-                                Text("Birthday: \(record.year)-\(record.month)-\(record.day)") // 誕生日の表示
-                                Text("Blood Type: \(record.bloodType)") // 血液型の表示
-                                Text("Saved on: \(record.today)") // 保存日付の表示
-                            }
-                        }
-                    }
-                     
-                    */
-
-                    // Fortune Responsesセクション
                     Section(header: Text("Fortune Responses")) {
-                        // fortuneResponsesの各レコードを表示
                         ForEach(fortuneResponses) { response in
                             VStack(alignment: .leading) {
                                 HStack {
-                                    // 画像の表示
                                     if let url = URL(string: response.logo_url.absoluteString) {
                                         WebImage(url: url)
-                                            .onSuccess { image, data, cacheType in
-                                                // 成功時の処理
-                                            }
+                                            .onSuccess { image, data, cacheType in }
                                             .resizable()
-                                            .indicator(.activity) // インジケータを表示
+                                            .indicator(.activity)
                                             .frame(width: 100, height: 100)
                                             .scaledToFit()
                                     }
                                     
-                                    Divider() // 縦線の表示
+                                    Divider()
                                     
                                     VStack(alignment: .leading) {
-                                        Text(response.name) // 名前の表示
+                                        Text(response.name)
                                             .font(.headline)
-                                        Text("Capital: \(response.capital)") // 首都の表示
-                                        Text("Has Coast Line: \(response.has_coast_line ? "Yes" : "No")") // 海岸線の有無の表示
-                                        Text("Citizen Day: \(response.CitizenDay_month)/\(response.CitizenDay_day)") // 県民の日の表示
-                                        Text(response.brief) // 簡単な説明の表示
+                                        Text("Capital: \(response.capital)")
+                                        Text("Has Coast Line: \(response.has_coast_line ? "Yes" : "No")")
+                                        Text("Citizen Day: \(response.CitizenDay_month)/\(response.CitizenDay_day)")
+                                        Text(response.brief)
                                             .lineLimit(3)
                                     }
                                 }
                             }
                             .listRowSeparator(.visible)
+                            .onTapGesture {
+                                selectedRecord = response
+                                showingDetail = true
+                            }
                         }
                     }
                 }
-                .listStyle(GroupedListStyle()) // グループ化されたリストスタイルを適用
+                .listStyle(GroupedListStyle())
+            }
+            .navigationDestination(isPresented: $showingDetail) {
+                if let record = selectedRecord {
+                    DetailView(personalRecord: personalRecords.first { $0.id == record.id }, fortune: record)
+                }
             }
         }
     }
